ce90158b3b8c47f07008ca2496832a6561df9bd6
Michael Ilett <michael.ilett@metro.co.uk>
2019-11-07 14:38:08 +0000

    Adding feed title and option to remove top pagination

--- src/react/components/Entries.js
+++ src/react/components/Entries.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 
 import EntryContainer from '../containers/EntryContainer';
 
-const Entries = ({ loading, entries }) => (
-  <div className={loading ? 'liveblog-feed is-loading' : 'liveblog-feed'}>
-    {
-      entries.length === 0 && !loading
-        ? <div className="liveblog-empty-message">There are no entries on this page.</div>
-        : entries.map(entry => <EntryContainer entry={entry} key={entry.id} />)
-    }
-  </div>
+const Entries = ({ title, loading, entries }) => (
+  <React.Fragment>
+    {title && <h3 className="liveblog-feed-title" >{title}</h3>}
+    <div className={loading ? 'liveblog-feed is-loading' : 'liveblog-feed'}>
+      {
+        entries.length === 0 && !loading
+          ? <div className="liveblog-empty-message">There are no entries on this page.</div>
+          : entries.map(entry => <EntryContainer entry={entry} key={entry.id} />)
+      }
+    </div>
+  </React.Fragment>
 );
 
 Entries.propTypes = {
+  title: PropTypes.string,
   entries: PropTypes.array,
   loading: PropTypes.bool,
 };
